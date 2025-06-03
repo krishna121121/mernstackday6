c472b6546f6c94454b04c490a4a9fe5c833df7a7
krishna121121 <krishnamadhavjha121@gmail.com>
2025-06-04 00:19:06 +0530

    this is task1 of day 6

---
+++ task1/t1.js
@@ -0,0 +1,9 @@
+let a=prompt("give me your age");
+
+if (a>=18){
+    document.write("yaa congratualtion <br>" );
+    document.write("you are eligible for voting");
+}
+else{
+    document.write("sorry you are not eligible for voting <br>");
+}
