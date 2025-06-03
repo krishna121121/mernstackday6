4447c13bb92895175a594a735d3f05f86ba706b0
krishna121121 <krishnamadhavjha121@gmail.com>
2025-06-04 00:31:05 +0530

    this is task2 of day 6

---
+++ task2/t2.js
@@ -0,0 +1,8 @@
+let count=prompt("write no of time you want to print")
+let str=prompt("write the name you want to print")
+let sum=1;
+
+while(count>=sum){
+    document.write(sum+". "+str+"<br>")
+    sum++
+}
